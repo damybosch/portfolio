0401c340324518708816443f40a9b13a28f4bef9
Damy Bosch <damybosch96@gmail.com>
2019-03-21 11:29:04 +0100

    added uikit

--- index.php
+++ index.php
@@ -5,9 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Damy Bosch Portfolio</title>
+
+    <!-- Style Scripts -->
+    <link rel="stylesheet" href="css/uikit.min.css" />
+
+    <!-- Javascripts -->
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
 </head>
 <body>
-    <h1>Dit is mijn portfolio</h1>
-    <h2>Damy Bosch</h2>
+<nav class="uk-navbar-container uk-navbar-transparent" uk-navbar="mode: hover">
+    <div class="uk-navbar-right">
+    <!-- <a class="uk-navbar-toggle" uk-navbar-toggle-icon href=""></a> -->
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="">home</a></li>
+            <li class="uk-parent"><a href="">skills
+                <div class="uk-navbar-dropdown" uk-drop="boundary: !nav; boundary-align: true; pos: bottom-justify;">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li class="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                    </ul>
+                </div>
+            </a></li>
+            <li><a href="">about</a></li>
+            <li><a href="">contact</a></li>
+        </ul>
+    </div>
+</nav>
+    <h1>Damy Bosch</h1>
 </body>
 </html>
